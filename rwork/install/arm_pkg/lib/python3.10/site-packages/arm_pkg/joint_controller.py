8cced7e4d2b5a32300556e22b6431325d2d6c20f
8kyu <guillemsenabre@gmail.com>
2023-11-01 15:04:26 +0800

    package built

--- rwork/install/arm_pkg/lib/python3.10/site-packages/arm_pkg/joint_controller.py
+++ rwork/install/arm_pkg/lib/python3.10/site-packages/arm_pkg/joint_controller.py
@@ -7,8 +7,8 @@ from std_msgs.msg import Float64
 class JointTorqueController(Node):
     def __init__(self):
         super().__init__('joint_torque_controller')
-        self.publisher_joint0 = self.create_publisher(Float64, '/two_joint_arm/joint0/pos_eff', 10)
-        self.publisher_joint1 = self.create_publisher(Float64, '/two_joint_arm/joint1/pos_eff', 10)
+        self.publisher_joint0 = self.create_publisher(Float64, '/two_joint_arm/joint0/pos_eff', 1)
+        self.publisher_joint1 = self.create_publisher(Float64, '/two_joint_arm/joint1/pos_eff', 1)
         
         self.timer = self.create_timer(1, self.move_joints)
 
@@ -21,7 +21,7 @@ class JointTorqueController(Node):
         msg_joint0 = Float64()
         msg_joint1 = Float64()
         msg_joint0.data = 0.5 * math.sin(self.angle)# Apply a constant torque of x Nm
-        msg_joint1.data = 0.3 * math.sin(self.angle)
+        msg_joint1.data = 10 * math.sin(self.angle)
         self.publisher_joint0.publish(msg_joint0)
         self.publisher_joint1.publish(msg_joint1)
 
