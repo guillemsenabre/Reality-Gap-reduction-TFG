c1f2c0fcf23737f2f5911ea840f1d2b76c01b56e
8kyu <guillemsenabre@gmail.com>
2023-11-13 15:25:07 +0800

    states node updated

--- rwork/src/arm_pkg/arm_pkg/state.py
+++ rwork/src/arm_pkg/arm_pkg/state.py
@@ -3,28 +3,11 @@ from rclpy.node import Node
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import Pose
 
-class JointStateListener(Node):
+class robotStates(Node):
     def __init__(self):
-        super().__init__('joint_state_listener')
+        super().__init__('robot_states')
 
         self.get_logger().info('Starting Node')
-
-        # create_subscription is from class Node (rclpy.node) -->
-        # create_subscription(
-        #   <message type>,
-        #   <topic to subscribe>,
-        #   <call to custom function>,
-        #   queue size (how many messages can be stored in the queue if they
-        # arrive faster than they're being processed),
-        # )
-
-        '''
-        self.subscription = self.create_subscription(
-            JointState,
-            '/world/two_joint_arm_world/model/two_joint_arm/joint_state',
-            self.check_data,
-            10)
-        '''
 
         self.subscription = self.create_subscription(
             JointState,
@@ -32,22 +15,12 @@ class JointStateListener(Node):
             self.check_data,
             5)
 
-    def listener_callback(self, msg):
-        self.get_logger().info('Received joint states:')
-        for name, position in zip(msg.name, msg.position):
-            self.get_logger().info(f'Joint: {name}, Position: {position}')
-    
-    def check_data(self, msg):
-        if msg:
-            self.listener_callback(msg)
-        else:
-            self.get_logger().info('No data to process')
 
 def main(args=None):
     rclpy.init(args=args)
-    joint_state_listener = JointStateListener()
-    rclpy.spin(joint_state_listener)
-    joint_state_listener.destroy_node()
+    robot_states = robotState()
+    rclpy.spin(robot_states)
+    robot_states.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
