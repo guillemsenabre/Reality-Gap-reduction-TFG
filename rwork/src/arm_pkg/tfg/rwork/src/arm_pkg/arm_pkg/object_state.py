a29a02700062f10e118e6bdc4c4b2af0b64d2d99
8kyu <guillemsenabre@gmail.com>
2023-11-14 20:41:19 +0800

    object states node updated

--- rwork/src/arm_pkg/tfg/rwork/src/arm_pkg/arm_pkg/object_state.py
+++ rwork/src/arm_pkg/tfg/rwork/src/arm_pkg/arm_pkg/object_state.py
@@ -6,19 +6,11 @@ from geometry_msgs.msg import PoseArray
 
         ######  STATE CLASS  ######
 
-class robotState(Node):
+class objectState(Node):
     def __init__(self):
-        super().__init__('robot_state')
+        super().__init__('object_state')
 
-        self.get_logger().info('Starting Node')
-
-        # Storage for latest data
-
-        self.latest_joint_state_1 = None
-        self.latest_joint_state_2 = None
-
-        self.latest_end_effector_pose_1 = None
-        self.latest_end_effector_pose_2 = None
+        self.get_logger().info('Starting Object State Node')
 
         # Subscriptions
 
@@ -27,20 +19,6 @@ class robotState(Node):
             '/world/full_env/dynamic_pose/info',
             self.gripper_pose,
             1)
-
-        self.j1_subscription = self.create_subscription(
-            JointState,
-            '/world/full_env/model/arm_1/joint_state',
-            self.joint_angles_1,
-            1)
-        
-        self.j2_subscription = self.create_subscription(
-            JointState,
-            '/world/full_env/model/arm_2/joint_state',
-            self.joint_angles_2,
-            1)
-
-        
 
         ######  GRIPPER GENERAL COORDENATES FUNCTIONS ######
     
@@ -62,28 +40,6 @@ class robotState(Node):
         self.latest_end_effector_pose_1 = self.extract_coordinates(msg.poses[15])
         self.latest_end_effector_pose_2 = self.extract_coordinates(msg.poses[27])
         self.update_robot_state()
-
-        ######  GRIPPER GENERAL COORDENATES FUNCTIONS ######
-
-    def joint_angles_1(self, msg):
-        self.latest_joint_state_1 = {name: position for name, position in zip(msg.name, msg.position)}
-        self.update_robot_state()
-
-    def joint_angles_2(self, msg):
-        self.latest_joint_state_2 = {name: position for name, position in zip(msg.name, msg.position)}
-        self.update_robot_state()
-
-
-        ###### SYNCHRONIZATION FUNCTION ######
-
-    def update_robot_state(self):
-        if self.latest_joint_state_1 and self.latest_end_effector_pose_1:
-            robot_state_1 = (self.latest_joint_state_1, self.latest_end_effector_pose_1)
-            self.get_logger().info(f'Robot 1 State: {robot_state_1}')
-
-        if self.latest_joint_state_2 and self.latest_end_effector_pose_2:
-            robot_state_2 = (self.latest_joint_state_2, self.latest_end_effector_pose_2)
-            self.get_logger().info(f'Robot 2 State: {robot_state_2}')    
 
 
         ######  INITIALIZATION FUNCTIONS ######
