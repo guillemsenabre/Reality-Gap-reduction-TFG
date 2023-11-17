767ee374470587513da3c161e3fa3c20359e10e3
8kyu <guillemsenabre@gmail.com>
2023-11-17 17:07:29 +0800

    added manhattan distances

--- rwork/src/arm_pkg/arm_pkg/drl/reward_function.py
+++ rwork/src/arm_pkg/arm_pkg/drl/reward_function.py
@@ -7,8 +7,11 @@ class Reward(Node):
         super().__init__('reward_function')
 
 
-    def manhattan_distance(self, gripper1_pos, gripper2_pos, object_pos):
-        return None
+    def manhattan_distance(self, g1_pos, g2_pos, obj_pos):
+        
+        rg1 = abs(g1_pos[0] - obj_pos[0]) + abs(g1_pos[1] - obj_pos[1]) + abs(g1_pos[2] - obj_pos[2])
+
+        rg2 = abs(g2_pos[0] - obj_pos[0]) + abs(g2_pos[1] - obj_pos[1]) + abs(g2_pos[2] - obj_pos[2])
     
     def object_deviation(self):
         return None
