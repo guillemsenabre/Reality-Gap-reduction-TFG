46344fdef156541ee0138030b10993f68766cbde
8kyu <guillemsenabre@gmail.com>
2023-11-18 21:02:37 +0800

    updates

--- rwork/install/arm_pkg/lib/python3.10/site-packages/arm_pkg/drl/reward_function.py
+++ rwork/install/arm_pkg/lib/python3.10/site-packages/arm_pkg/drl/reward_function.py
@@ -5,7 +5,8 @@ from ros_gz_interfaces.msg import Float32Array
 class Reward(Node):
     def __init__(self):
         super().__init__('reward_function')
-        self.get_logger().info('Starting reward function node')
+        self.get_logger().info('Starting reward function node ...')
+
 
         self.state_subscription = self.create_subscription(
             Float32Array,
@@ -14,11 +15,34 @@ class Reward(Node):
             1
         )
 
-    def states_unpack(self, msg: Float32Array):
-        data = msg.data
-        #print(f'Received State Data: {data}')
+        self.get_logger().info('Waiting for data ...')
 
-        print(f'robot 1 data: {data[0:10]}')
+    def states_unpack(self, msg: Float32Array):
+
+        '''
+        
+                                    Data structure
+
+        [
+            'f',
+            J01, J11, J21, J31, EX1, EY1, EZ1, EI1, EJ1, EK1,EW1,   --> ROBOT 1
+            J02, J12, J22, J32, EX2, EY2, EZ2, EI2, EJ2, EK2,EW2,   --> ROBOT 2  
+            OBJX, OBJY, OBJZ, OBJI, OBJJ, OBJK, OBJW                --> OBJECT
+        ] 
+
+        '''
+                
+
+        data = msg.data
+
+        gripper_1_pos = data[5:7]
+        gripper_2_pos = data[16:18]
+        object_pos = data[23:29]
+
+        # Log gripper and object positions for debugging
+        self.get_logger().info('Gripper 1 Position: {}'.format(gripper_1_pos))
+        self.get_logger().info('Gripper 2 Position: {}'.format(gripper_2_pos))
+        self.get_logger().info('Object Position: {}'.format(object_pos))
 
 
     def manhattan_distance(self, g1_pos, g2_pos, obj_pos):
@@ -27,8 +51,15 @@ class Reward(Node):
 
         rg2 = abs(g2_pos[0] - obj_pos[0]) + abs(g2_pos[1] - obj_pos[1]) + abs(g2_pos[2] - obj_pos[2])
     
+
+
+
+
+
+
+    
     def object_deviation(self):
-        return None
+        pass
 
 
 
