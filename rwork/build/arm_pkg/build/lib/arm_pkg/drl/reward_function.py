e8beed95d1ad71e4eda3dac961ee3a72537aad67
8kyu <guillemsenabre@gmail.com>
2023-11-19 15:21:35 +0800

    package built

--- rwork/build/arm_pkg/build/lib/arm_pkg/drl/reward_function.py
+++ rwork/build/arm_pkg/build/lib/arm_pkg/drl/reward_function.py
@@ -1,12 +1,21 @@
 import rclpy
 from rclpy.node import Node
 from ros_gz_interfaces.msg import Float32Array
+from std_msgs.msg import Float32
 
 class Reward(Node):
     def __init__(self):
         super().__init__('reward_function')
         self.get_logger().info('Starting reward function node ...')
 
+        # Publisher
+
+        self.reward_publisher = self.create_publisher(
+            Float32,
+            'packed//data',
+            1)
+        
+        # Subscriber
 
         self.state_subscription = self.create_subscription(
             Float32Array,
@@ -48,8 +57,8 @@ class Reward(Node):
 
         rg2 = abs(gripper_2_pos[0] - object_2_pos[0]) + abs(gripper_2_pos[1] - object_2_pos[1]) + abs(gripper_2_pos[2] - object_2_pos[2])
     
-        self.get_logger().info(f'reward distance 1: {rg1}')
-        self.get_logger().info(f'reward distance 2: {rg2}')
+        #self.get_logger().info(f'reward distance 1: {rg1}')
+        #self.get_logger().info(f'reward distance 2: {rg2}')
 
         return -(rg1 + rg2)
         
@@ -59,6 +68,15 @@ class Reward(Node):
         distance_reward = self.manhattan_distance(msg)
 
         self.get_logger().info(f'Distance Reward: {distance_reward}')
+
+
+
+
+
+
+
+    
+        self.reward_publisher(Float32(data=distance_reward))
 
 
     
