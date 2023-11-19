3b58accd964e8e527e704f98bddba5880f14538e
8kyu <guillemsenabre@gmail.com>
2023-11-19 19:43:26 +0800

    updates on training loop

--- rwork/src/arm_pkg/arm_pkg/drl/ddpg.py
+++ rwork/src/arm_pkg/arm_pkg/drl/ddpg.py
@@ -126,6 +126,11 @@ class RosData(Node):
             1
         )
 
+        # Initialize the DDPG agent
+        state_dim = 12  
+        action_dim = 8
+        agent = DDPGAgent(state_dim, action_dim)
+
     def process_state_data(self, msg: Float32Array):
 
         data = msg.data
@@ -135,20 +140,23 @@ class RosData(Node):
         gripper_2_pos = data[15:18]
         object_pos = data[22:25]
 
-        self.state = gripper_1_pos + gripper_2_pos + object_pos
+        object_1_pos = [object_pos[0] - 0.125, object_pos[1], object_pos[2]]
+        object_2_pos = [object_pos[0] + 0.125, object_pos[1], object_pos[2]]
+
+        self.state = gripper_1_pos + gripper_2_pos + object_1_pos + object_2_pos
 
     def process_reward_data(self, msg: Float32):
         self.reward_value = msg.data
+
+    
+    def move_joints(self, action):
+
 
 
 def main(args=None):
     rclpy.init(args=args)
     ros_data = RosData()
 
-        # Initialize the DDPG agent
-    state_dim = 18  # Modify based on your state space
-    action_dim = 6  # Modify based on your action space
-    agent = DDPGAgent(state_dim, action_dim)
 
     # Training loop
 
@@ -157,16 +165,20 @@ def main(args=None):
     for episode in range(num_episodes):
         # Reset environment and get initial state
         state = env.reset()
+        state = 
 
         for step in range(max_steps):
             # Select action from the agent's policy
-            action = agent.select_action(state)
+            action = DDPGAgent.select_action(state)
 
-            # Execute action and observe next state and reward
+            ## EXECUTE ACTION HERE ##
+
+            RosData.move_joints(action)
+            # observe next state and reward
             next_state, reward, done, _ = env.step(action)
 
             # Update agent
-            agent.update(state, action, reward, next_state, done)
+            DDPGAgent.update(state, action, reward, next_state, done)
 
             # Move to the next state
             state = next_state
