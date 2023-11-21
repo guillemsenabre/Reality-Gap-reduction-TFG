232ce5dbcd315bd7b2e2a0936462a54e7ccb25e4
8kyu <guillemsenabre@gmail.com>
2023-11-21 14:38:32 +0800

    test reset is working?

--- rwork/src/arm_pkg/arm_pkg/tests/launchgazebotest.py
+++ rwork/src/arm_pkg/arm_pkg/tests/launchgazebotest.py
@@ -1,4 +1,19 @@
 import subprocess
+import os
 
+# Get the full path to the home directory
+home_directory = os.path.expanduser("~")
 
-subprocess.run(['ign', 'gazebo', '~/tfg/rowrk/src/sdf_files/full_env_simpler.sdf'], check=True)
+# Full path to the SDF file
+sdf_file_path = os.path.join(home_directory, 'tfg', 'rwork', 'src', 'sdf_files', 'full_env_simpler.sdf')
+
+# Run Gazebo subprocess
+try:
+    result = subprocess.run(['ign', 'gazebo', sdf_file_path], check=True, stderr=subprocess.PIPE)
+except subprocess.CalledProcessError as e:
+    print("Error running Gazebo:")
+    print(e.stderr.decode())
+    raise  # Re-raise the exception to terminate the script
+
+# If no error occurred, print the output (if needed)
+print(result.stdout.decode())
