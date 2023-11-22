bcf7144c31ee512b3a39021effdfcc41cff671eb
8kyu <guillemsenabre@gmail.com>
2023-11-23 00:51:07 +0800

    package built

--- rwork/build/arm_pkg/prefix_override/sitecustomize.py
+++ rwork/build/arm_pkg/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/chi8kyu/tfg/rwork/install/arm_pkg'
+sys.prefix = sys.exec_prefix = '/home/kyu8/tfg/rwork/install/arm_pkg'
