e15e7f0130fd4d45da2daf1c346ee55073976584
8kyu <guillemsenabre@gmail.com>
2023-12-01 22:01:28 +0800

    package built

--- rwork/install/arm_pkg/lib/python3.10/site-packages/arm_pkg/drl/training_loop.py
+++ rwork/install/arm_pkg/lib/python3.10/site-packages/arm_pkg/drl/training_loop.py
@@ -250,12 +250,14 @@ class RosData(Node):
         self.reward_value = msg.data
 
     def terminal_condition(self):
-        list = self.reward_list
-        values = self.reward_value
-        list.append(values)
-        if self.maximum_accumulative_reward == len(list):
-            not_change = list[0] == list[self.maximum_accumulative_reward - 1] or ((self.state[11] or self.state[8]) < 1.2)
+        self.reward_list.append(self.reward_value)
+        if self.maximum_accumulative_reward == len(self.reward_list):
+
+            #FIXME - CORRECT REWARD LIST INSTANCE
+            not_change = list[0] == list[-1]
             self.maximum_accumulative_reward = 0
+        elif (self.state[11] or self.state[8]) < 1.2:
+            not_change = True
         else:
             not_change = False
         
