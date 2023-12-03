149d5541e742baa16a6374f12bb759a242998af8
8kyu <guillemsenabre@gmail.com>
2023-12-03 20:08:39 +0800

    debugging

--- rwork/src/arm_pkg/arm_pkg/drl/training_loop.py
+++ rwork/src/arm_pkg/arm_pkg/drl/training_loop.py
@@ -271,25 +271,15 @@ class RosData(Node):
 
     def terminal_condition(self):
         self.reward_list.append(self.reward_value)
-        
-        print(self.maximum_accumulative_reward)
-        print(len(self.reward_list))
 
         if self.maximum_accumulative_reward == len(self.reward_list):
-            print("Accumulative checking...")
-
             margin = abs(self.margin_value * self.reward_list[0])
-            print(margin)
-
             difference = abs(self.reward_list[0] - self.reward_list[-1]) 
-            print(difference)
-
             self.reward_list = []
 
             if difference <= margin:
                 return True
-            
-            
+                        
         elif (self.state[11] or self.state[8]) < 1.2:
             return True
         
