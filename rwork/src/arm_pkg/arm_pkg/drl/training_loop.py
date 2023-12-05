0456e9e2a0b57dd55a427f6b0bf8cb764f5d84f2
8kyu <guillemsenabre@gmail.com>
2023-12-05 21:24:56 +0800

    corrections

--- rwork/src/arm_pkg/arm_pkg/drl/training_loop.py
+++ rwork/src/arm_pkg/arm_pkg/drl/training_loop.py
@@ -336,7 +336,33 @@ class RosData(Node):
 
 #!SECTION
 
+#SECTION - Plotting
 
+def plot_results(episode_rewards, actor_losses, critic_losses):
+    # Flatten the list of lists into a single list
+    flattened_rewards = [reward for episode in episode_rewards for reward in episode]
+
+    # Plot episode rewards
+    plt.figure(figsize=(12, 6))
+
+    plt.plot(flattened_rewards, label='Episode Total Reward')
+    plt.title('Episode Rewards')
+    plt.xlabel('Step')
+    plt.ylabel('Total Reward')
+    plt.legend()
+
+    # Plot actor and critic losses
+    plt.plot(actor_losses, label='Actor Loss', alpha=0.7)
+    plt.plot(critic_losses, label='Critic Loss', alpha=0.7)
+    plt.title('Actor and Critic Losses')
+    plt.xlabel('Step')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+#!SECTION
 
 #SECTION - TRAINING LOOP -
 
@@ -395,34 +421,6 @@ def main(args=None):
 
     ros_data.destroy_node()
     rclpy.shutdown()
-
-#!SECTION
-
-#SECTION - Plotting
-
-def plot_results(episode_rewards, actor_losses, critic_losses):
-    # Flatten the list of lists into a single list
-    flattened_rewards = [reward for episode in episode_rewards for reward in episode]
-
-    # Plot episode rewards
-    plt.figure(figsize=(12, 6))
-
-    plt.plot(flattened_rewards, label='Episode Total Reward')
-    plt.title('Episode Rewards')
-    plt.xlabel('Step')
-    plt.ylabel('Total Reward')
-    plt.legend()
-
-    # Plot actor and critic losses
-    plt.plot(actor_losses, label='Actor Loss', alpha=0.7)
-    plt.plot(critic_losses, label='Critic Loss', alpha=0.7)
-    plt.title('Actor and Critic Losses')
-    plt.xlabel('Step')
-    plt.ylabel('Loss')
-    plt.legend()
-
-    plt.tight_layout()
-    plt.show()
 
 #!SECTION
 #!SECTION
