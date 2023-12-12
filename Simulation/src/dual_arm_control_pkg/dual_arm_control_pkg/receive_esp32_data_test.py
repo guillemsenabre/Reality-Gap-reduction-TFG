8c65eaaab7c3e3cfc6b6e0dcbcffa5b4ec777567
8kyu <guillemsenabre@gmail.com>
2023-12-12 14:03:59 +0800

    COM serial updated and working --> /dev/ttyUSB0

--- Simulation/src/dual_arm_control_pkg/dual_arm_control_pkg/receive_esp32_data_test.py
+++ Simulation/src/dual_arm_control_pkg/dual_arm_control_pkg/receive_esp32_data_test.py
@@ -1,9 +1,9 @@
 import serial
 import rclpy
 
-
+port_name = '/dev/ttyUSB0' 
 # Open the serial connection (adjust port and baudrate based on your ESP32 configuration)
-ser = serial.Serial('COM0', 115200)
+ser = serial.Serial(port_name, 115200)
 
 try:
     while True:
