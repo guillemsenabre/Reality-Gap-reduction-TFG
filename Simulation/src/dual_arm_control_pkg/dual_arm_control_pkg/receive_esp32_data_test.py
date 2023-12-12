1b06ab5f35565bced83f7db8345b65ca9a13eb42
8kyu <guillemsenabre@gmail.com>
2023-12-12 13:51:19 +0800

    node filled

---
+++ Simulation/src/dual_arm_control_pkg/dual_arm_control_pkg/receive_esp32_data_test.py
@@ -0,0 +1,16 @@
+import serial
+import rclpy
+
+
+# Open the serial connection (adjust port and baudrate based on your ESP32 configuration)
+ser = serial.Serial('COMx', 115200)
+
+try:
+    while True:
+        # Read a line from the serial port
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+except KeyboardInterrupt:
+    pass
+finally:
+    ser.close()
