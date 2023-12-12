7faeac81ceea6ad20bcc8cf2d59fff2312f36cee
8kyu <guillemsenabre@gmail.com>
2023-12-12 16:59:14 +0800

    package built

--- build/arm_pkg/build/lib/arm_pkg/drl/ddpg.py
+++ build/arm_pkg/build/lib/arm_pkg/drl/ddpg.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from rbuffer import ReplayBuffer
+from .rbuffer import ReplayBuffer
 
 #SECTION - POLICY DRL ALGORITHM -
 
