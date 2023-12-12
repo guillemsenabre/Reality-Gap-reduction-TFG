265ce89ed30c5c608bb0c08b4ff99fb40fbd532e
8kyu <guillemsenabre@gmail.com>
2023-12-12 14:31:25 +0800

    C++ file to send esp32 data through socket and WiFi

---
+++ Implementation/tests/WiFi com/socket_test/socket_test.ino
@@ -0,0 +1,57 @@
+#include <WiFi.h>
+
+const char *ssid = "your-ssid";      // Your Wi-Fi SSID
+const char *password = "your-password";  // Your Wi-Fi password
+
+const int trigPin = 32;
+const int echoPin = 33;
+
+#define SOUND_SPEED 0.034
+WiFiServer server(80);
+
+float distanceCm;
+long duration;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+
+  // Print ESP32 IP address
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+}
+
+void loop() {
+  WiFiClient client = server.available();
+
+  if (client) {
+    Serial.println("New client connected");
+    distanceCm = getDistance();  // Replace with your actual distance calculation function
+
+    // Send the distance data to the client
+    client.print("Distance (cm): ");
+    client.print(distanceCm);
+    client.print(",Distance (inch): ");
+    client.println(distanceCm * 0.393701);
+
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
+
+float getDistance() {
+  // Implement your distance calculation logic here
+  // For example, using ultrasonic sensor or any other distance measurement method
+  return 42.0;  // Replace with your actual distance calculation
+}
