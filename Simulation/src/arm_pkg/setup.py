873568449aba30ce94d91d7310b60a015ac3efd4
8kyu <guillemsenabre@gmail.com>
2023-12-12 16:15:08 +0800

    addede rbuffer to install required

--- Simulation/src/arm_pkg/setup.py
+++ Simulation/src/arm_pkg/setup.py
@@ -11,7 +11,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools'
+        'rbuffer'
+        ],
     zip_safe=True,
     maintainer='kyu8',
     maintainer_email='guillemsenabre@gmail.com',
