b5fcfdbcb7b5cf1e7e87aa7339a77dd176eb7b67
8kyu <guillemsenabre@gmail.com>
2023-12-14 14:22:09 +0800

    inference files moved inside the ros2 workspace

---
+++ Simulation/src/arm_pkg/arm_pkg/drl/inference_files/inference.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+from ..ddpg import DDPGAgent
+
+model_path = "./models/"
+saved_model_filename = "your_ddpg_model.pth"
+
+
+ddpg_model = DDPGAgent()
+ddpg_model.load_state_dict(torch.load(model_path + saved_model_filename))
+ddpg_model.eval()
+
+for param in ddpg_model.layer1.parameters():
+    param.requires_grad = False
+
+for param in ddpg_model.layer2.parameters():
+    param.requires_grad = False
+
