5ad1009b305bef702dc91ec0bbc28a2035712a1a
8kyu <guillemsenabre@gmail.com>
2023-12-15 19:56:46 +0800

    package built

--- Simulation/build/dual_arm_control_pkg/prefix_override/sitecustomize.py
+++ Simulation/build/dual_arm_control_pkg/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/chi8kyu/tfg/Simulation/install/dual_arm_control_pkg'
+sys.prefix = sys.exec_prefix = '/home/kyu8/tfg/Simulation/install/dual_arm_control_pkg'
