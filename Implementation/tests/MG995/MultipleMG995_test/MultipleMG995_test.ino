19f42ee25698fc64166356533923903d5c098a22
8kyu <guillemsenabre@gmail.com>
2023-12-18 17:29:02 +0800

    script completed

--- Implementation/tests/MG995/MultipleMG995_test/MultipleMG995_test.ino
+++ Implementation/tests/MG995/MultipleMG995_test/MultipleMG995_test.ino
@@ -2,6 +2,7 @@
 
 #define SERVO1_PIN 26
 #define SERVO2_PIN 27
+#define MULTIPLIER -1
 
 Servo servoMotor1;
 Servo servoMotor2;
@@ -13,13 +14,16 @@ void setup() {
 
 void loop() {
   for (int pos = 0; pos <= 180; pos += 1) {
-    servoMotor.write(pos);
-
+    int pos2 = pos*MULTIPLIER;
+    servoMotor1.write(pos);
+    servoMotor2.write(pos2);
     delay(15);
   }
 
   for (int pos = 180; pos >= 0; pos -= 1) {
-    servoMotor.write(pos);
+    int pos2 = pos*MULTIPLIER;
+    servoMotor1.write(pos2);
+    servoMotor2.write(pos);
     delay(15);
   }
 }
