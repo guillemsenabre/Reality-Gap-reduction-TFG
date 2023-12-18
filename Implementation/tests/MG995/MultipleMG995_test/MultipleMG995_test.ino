c02f5899193eedd3bb2fdbfc19506742058e81e5
8kyu <guillemsenabre@gmail.com>
2023-12-18 19:14:30 +0800

    multiple servo file

--- Implementation/tests/MG995/MultipleMG995_test/MultipleMG995_test.ino
+++ Implementation/tests/MG995/MultipleMG995_test/MultipleMG995_test.ino
@@ -2,28 +2,56 @@
 
 #define SERVO1_PIN 26
 #define SERVO2_PIN 27
-#define MULTIPLIER -1
 
 Servo servoMotor1;
 Servo servoMotor2;
 
 void setup() {
+  Serial.begin(115200);
+  while(!Serial);
   servoMotor1.attach(SERVO1_PIN);
   servoMotor2.attach(SERVO2_PIN);
 }
 
+int getMotorAngle(Servo servo) {
+  return servo.read();
+}
+
 void loop() {
   for (int pos = 0; pos <= 180; pos += 1) {
-    int pos2 = pos*MULTIPLIER;
     servoMotor1.write(pos);
-    servoMotor2.write(pos2);
-    delay(15);
-  }
-
-  for (int pos = 180; pos >= 0; pos -= 1) {
-    int pos2 = pos*MULTIPLIER;
-    servoMotor1.write(pos2);
     servoMotor2.write(pos);
-    delay(15);
+
+    int angle1 = servoMotor1.read();
+    int angle2 = servoMotor2.read();
+
+    Serial.print("Servo 1 Angle: ");
+    Serial.print(angle1);
+    Serial.print("\tServo 2 Angle: ");
+    Serial.println(angle2);
+
+   // int angle1 = getMotorAngle(servoMotor1);
+   // int angle2 = getMotorAngle(servoMotor2);
+
+    delay(50);
+
+    
+  }
+  for (int pos = 180; pos >= 0; pos -= 1) {
+    servoMotor1.write(pos);
+    servoMotor2.write(pos);
+
+    // int angle1 = getMotorAngle(servoMotor1);
+    // int angle2 = getMotorAngle(servoMotor2);
+
+    int angle1 = servoMotor1.read();
+    int angle2 = servoMotor2.read();
+
+    Serial.print("Servo 1 Angle: ");
+    Serial.print(angle1);
+    Serial.print("\tServo 2 Angle: ");
+    Serial.println(angle2);
+    
+    delay(50);
   }
 }
