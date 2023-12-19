875e1c488e57127d598558945be2a9e2fd73092f
8kyu <guillemsenabre@gmail.com>
2023-12-20 00:21:32 +0800

    move joints updated with one single esp32

--- Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/move_joints.py
+++ Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/move_joints.py
@@ -1,14 +1,23 @@
 import serial
+import time
 
-class MoveJoints():
-    def __init__(self, port1, port2):
-        self.ser1 = serial.Serial(port1, baudrate=115200, timeout=1)
-        self.ser2 = serial.Serial(port2, baudrate=115200, timeout=1)
+class MoveJoints:
+    def __init__(self, port):
+        self.ser = serial.Serial(port, baudrate=115200, timeout=1)
 
-    def get_actions(self, actions):
-        pass
-    
-    def move_joints(self):
-        pass
+    def move_joints(self, actions):
+        if len(actions)%2 != 0:
+            print("Error: there has to be an even amount of values")
+            return
 
+        try:
+            self._send_list(self.ser, actions)
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            self.ser.close()
 
+    def _send_list(self, ser, values):
+        values_str = ','.join(map(str, values))
+        ser.write(values_str.encode())
+        print(f"Sent values to {ser.port}: {values_str}")
