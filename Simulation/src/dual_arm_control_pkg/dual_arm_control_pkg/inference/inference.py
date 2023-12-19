f28fa162db782b54044a79955312e299355c2f29
8kyu <guillemsenabre@gmail.com>
2023-12-19 17:34:39 +0800

    added file to get states and updated inference.py

--- Simulation/src/dual_arm_control_pkg/dual_arm_control_pkg/inference/inference.py
+++ Simulation/src/dual_arm_control_pkg/dual_arm_control_pkg/inference/inference.py
@@ -2,18 +2,24 @@ import torch
 import torch.nn as nn
 
 from arm_pkg.arm_pkg.drl.ddpg import DDPGAgent
-from duañ
 
-model_path =
-saved_model_filename = "your_ddpg_model.pth"  
-
-
+model_path = ""
 ddpg_model = DDPGAgent()
-ddpg_model.load_state_dict(torch.load(model_path + saved_model_filename))
-ddpg_model.eval()
 
-for param in ddpg_model.layer1.parameters():
-    param.requires_grad = False
+train_or_pretrained = input("Do you want to 'train' or use a 'pretrained' model?")
 
-for param in ddpg_model.layer2.parameters():
-    param.requires_grad = False
+if train_or_pretrained == "pretrained":
+    print(f"using pretrained model located in {model_path} ...")
+
+    ddpg_model.load_state_dict(torch.load(model_path))
+    ddpg_model.eval()
+
+    for param in ddpg_model.layer1.parameters():
+        param.requires_grad = False
+
+    for param in ddpg_model.layer2.parameters():
+        param.requires_grad = False
+
+elif train_or_pretrained == "train":
+    print(f"training ddpg model from scratch...")
+    
