846f485f618da99a620b6cc3c770be94fda488f7
8kyu <guillemsenabre@gmail.com>
2023-12-19 19:23:34 +0800

    new method added to inference, to get pretrained model

--- Simulation/src/arm_pkg/arm_pkg/drl/inference.py
+++ Simulation/src/arm_pkg/arm_pkg/drl/inference.py
@@ -13,16 +13,11 @@ ddpg_model = DDPGAgent(config.state_dim, config.action_dim)
 
 def main():
     print("FUCKING STARTED")
-    model_name = config.model_name
-    model_path = os.path.expanduser(f'~/tfg/Simulation/src/models/{model_name}')
 
     train_or_pretrained = input("Do you want to 'train' or use a 'pretrained' model?")
-    
-    if train_or_pretrained == "pretrained":
-        print(f"using pretrained model located in {model_path} ...")
 
-        ddpg_model.load_state_dict(torch.load(model_path + model_name))
-        ddpg_model.eval()
+    if train_or_pretrained == "pretrained":
+        get_pretrained_model()
 
         for param in ddpg_model.layer1.parameters():
             param.requires_grad = False
@@ -41,6 +36,22 @@ def main():
     print("\nModel Layers:")
     for name, layer in ddpg_model.named_children():
         print(f"{name}: {layer}")
+
+
+def get_pretrained_model():
+    print(f"using pretrained model located in {model_path} ...")
+
+    model_name = config.model_name
+    model_path = os.path.expanduser(f'~/tfg/Simulation/src/models/{model_name}')
+        
+    # Load the state dictionaries
+    checkpoint = torch.load('ddpg_model.pth')
+
+    # Load the state dictionaries into the actor and critic models
+    ddpg_model.actor.load_state_dict(checkpoint['actor_state_dict'])
+    ddpg_model.critic.load_state_dict(checkpoint['critic_state_dict'])
+
+
 
 if __name__=='__main__':
     main()
