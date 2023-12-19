b5c150e276b5a16bd6af18c61172d05f9a188542
8kyu <guillemsenabre@gmail.com>
2023-12-19 17:46:14 +0800

    added model name and trhough config

--- Simulation/src/dual_arm_control_pkg/dual_arm_control_pkg/inference/inference.py
+++ Simulation/src/dual_arm_control_pkg/dual_arm_control_pkg/inference/inference.py
@@ -2,16 +2,21 @@ import torch
 import torch.nn as nn
 
 from arm_pkg.arm_pkg.drl.ddpg import DDPGAgent
+from arm_pkg.arm_pkg.drl.configuration import Configuration
 
-model_path = ""
+
 ddpg_model = DDPGAgent()
+config = Configuration()
+
+model_path = "../../../models/"
+model_name = config.model_name
 
 train_or_pretrained = input("Do you want to 'train' or use a 'pretrained' model?")
 
 if train_or_pretrained == "pretrained":
     print(f"using pretrained model located in {model_path} ...")
 
-    ddpg_model.load_state_dict(torch.load(model_path))
+    ddpg_model.load_state_dict(torch.load(model_path + model_name))
     ddpg_model.eval()
 
     for param in ddpg_model.layer1.parameters():
