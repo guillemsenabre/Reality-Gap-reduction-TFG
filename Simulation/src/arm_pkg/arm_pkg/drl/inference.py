5fdabae9aa0325e883e8a6f9963cdbe574931fe1
8kyu <guillemsenabre@gmail.com>
2023-12-19 19:25:22 +0800

    new method to deploy plain model

--- Simulation/src/arm_pkg/arm_pkg/drl/inference.py
+++ Simulation/src/arm_pkg/arm_pkg/drl/inference.py
@@ -14,28 +14,17 @@ ddpg_model = DDPGAgent(config.state_dim, config.action_dim)
 def main():
     print("FUCKING STARTED")
 
-    train_or_pretrained = input("Do you want to 'train' or use a 'pretrained' model?")
+    train_or_pretrained = input("Hey do you want to 'train' from scratch or use a 'pretrained' model?")
 
     if train_or_pretrained == "pretrained":
         get_pretrained_model()
 
-        for param in ddpg_model.layer1.parameters():
-            param.requires_grad = False
-
-        for param in ddpg_model.layer2.parameters():
-            param.requires_grad = False
-
     elif train_or_pretrained == "train":
         print(f"training ddpg model from scratch...")
+        get_plain_model()
 
-    # Print model parameters and layers
-    print("\nModel Parameters:")
-    for name, param in ddpg_model.named_parameters():
-        print(f"{name}: {param.shape}")
-
-    print("\nModel Layers:")
-    for name, layer in ddpg_model.named_children():
-        print(f"{name}: {layer}")
+    else:
+        print("STFU goodbye")
 
 
 def get_pretrained_model():
@@ -50,6 +39,9 @@ def get_pretrained_model():
     # Load the state dictionaries into the actor and critic models
     ddpg_model.actor.load_state_dict(checkpoint['actor_state_dict'])
     ddpg_model.critic.load_state_dict(checkpoint['critic_state_dict'])
+
+def get_plain_model():
+
 
 
 
