4c041df3e238a866145bf6d0b0f26728a2b172ae
8kyu <guillemsenabre@gmail.com>
2023-12-19 20:31:57 +0800

    Move updates

--- Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/move_joints.py
+++ Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/move_joints.py
@@ -1,5 +1,9 @@
-
+import serial
 
 class MoveJoints():
     def __init__(self, port1, port2):
+        self.ser1 = serial.Serial(port1, baudrate=115200, timeout=1)
+        self.ser2 = serial.Serial(port2, baudrate=115200, timeout=1)
+
+    def move_joints(self, actions):
         pass
