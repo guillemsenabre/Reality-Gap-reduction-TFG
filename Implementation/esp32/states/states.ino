022b11f7baac761409ca033971d9c71d1cbf259a
8kyu <guillemsenabre@gmail.com>
2023-12-20 13:32:13 +0800

    c++ states added

---
+++ Implementation/esp32/states/states.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+#include <Wire.h>
+#include <MPU6050_light.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
