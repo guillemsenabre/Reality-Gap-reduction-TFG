4f9dafafc5012e4b41880b42e9fe5b19ce5bccc5
8kyu <guillemsenabre@gmail.com>
2023-12-20 19:12:57 +0800

    added one terminal condition

--- Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/abort_save.py
+++ Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/abort_save.py
@@ -3,3 +3,8 @@ from reward import Reward
 class AbortOrSave():
     def __init__(self):
         pass
+
+    def reached_goal(self):
+
+        #Both distances are 0 for an amount of time.
+        pass
