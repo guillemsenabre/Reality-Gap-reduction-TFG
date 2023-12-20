25d8a90867bc177ddd21253b617e0396b03e2a0b
8kyu <guillemsenabre@gmail.com>
2023-12-20 18:12:07 +0800

    new method, dynamic velocity

--- Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/reward.py
+++ Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/reward.py
@@ -7,10 +7,29 @@ class Reward():
         self.distanceRB2 = self.states.read_sensor_data[11]
         self.object_orientation = self.states.read_sensor_data[12:14]
     
-    def distance_reward(self):
-        pass
+    def _distance_reward(self):
 
-    def orientation_reward(self):
+        # The smaller the distance the greater the reward (using f(x)=1/x, x>0)
+        reward1 = 1/self.distanceRB1 
+        reward2 = 1/self.distanceRB2
+
+        distance_value = reward1 + reward2
+
+        return distance_value
+
+    def _orientation_reward(self):
+
+
+        orientation_value = 
+        
+        return orientation_value
+
+    def _drop_velocity_reward(self):
+        # This function gets higher values as the velocity of the joints (
+        #or, what is the same, the how much the angle of a joint has changed per time)
+        #decreases when the distanceRB1 and distanceRB2 also decreases, to avoid 
+        #coliding with the object at high speed and, ideally, to stop the robots when 
+        #the distances are 0.
         pass
 
     def reward(self):
