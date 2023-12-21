ad18854bdddcb73c392093d0517d448854d8173f
8kyu <guillemsenabre@gmail.com>
2023-12-21 17:05:38 +0800

    fuck ros2

--- Simulation/src/arm_pkg/arm_pkg/drl/inference.py
+++ Simulation/src/arm_pkg/arm_pkg/drl/inference.py
@@ -6,13 +6,13 @@ import torch.nn as nn
 import os
 import time
 
-from .ddpg import DDPGAgent
-from .configuration import Configuration
-from .reset import Reset
-from .sub_modules.abort_save import AbortOrSave
-from .sub_modules.move_joints import MoveJoints
-from .sub_modules.states import States
-from .sub_modules.reward import Reward
+from ddpg import DDPGAgent
+from configuration import Configuration
+from reset import Reset
+from sub_modules.abort_save import AbortOrSave
+from sub_modules.move_joints import MoveJoints
+from sub_modules.states import States
+from sub_modules.reward import Reward
 
 class Inference(Node):
     def __init__(self):
