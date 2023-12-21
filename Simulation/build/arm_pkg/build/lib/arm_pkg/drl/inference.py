80dd960a413eac3b9f305c93dc7f665c17807488
8kyu <guillemsenabre@gmail.com>
2023-12-21 17:17:49 +0800

    package built, removed reset

--- Simulation/build/arm_pkg/build/lib/arm_pkg/drl/inference.py
+++ Simulation/build/arm_pkg/build/lib/arm_pkg/drl/inference.py
@@ -1,6 +1,3 @@
-import rclpy
-from rclpy.node import Node
-
 import torch
 import torch.nn as nn
 import os
@@ -14,48 +11,41 @@ from sub_modules.move_joints import MoveJoints
 from sub_modules.states import States
 from sub_modules.reward import Reward
 
-class Inference(Node):
+class Inference:
     def __init__(self):
-        super().__init__('inference')
-
-        self.get_logger().info("Starting inference node...")
+        print("Starting inference node...")
 
         time.sleep(100)
 
-        self.get_logger().info("Getting esp32 port...")
+        print("Getting esp32 port...")
 
-        self.port1 = self.config.port1
-
+        self.config = Configuration()
         self.move = MoveJoints()
         self.states = States()
         self.reward = Reward()
-        self.config = Configuration()
         #self.reset = Reset()
         self.abort = AbortOrSave()
         self.save = AbortOrSave()
         self.ddpg_model = DDPGAgent(self.config.state_dim, self.config.action_dim)
                 
-        train_or_pretrained = input("Hey do you want to 'train' from scratch or use a 'pretrained' model?")
+        train_or_pretrained = input("Hey, do you want to 'train' from scratch or use a 'pretrained' model? ")
 
         if train_or_pretrained == "pretrained":
-            self.get_logger().info("Getting pretrained model ready...")
+            print("Getting pretrained model ready...")
             self.get_pretrained_model()
             #self.train()??
 
         elif train_or_pretrained == "train":
-            self.get_logger().info((f"Training ddpg model from scratch..."))
+            print("Training ddpg model from scratch...")
             self.train(self.ddpg_model)
 
         else:
-            self.get_logger().info(("STFU goodbye"))
-
+            print("STFU goodbye")
 
     def get_pretrained_model(self):
-        self.get_logger().info((f"Using pretrained model located in {model_path} ..."))
-
         model_name = self.config.model_name
         model_path = os.path.expanduser(f'~/tfg/Simulation/src/models/{model_name}')
-            
+
         # Load the state dictionaries
         checkpoint = torch.load('ddpg_model.pth')
 
@@ -74,7 +64,6 @@ class Inference(Node):
             param.requires_grad = False
         for param in self.ddpg_model.critic.fc2.parameters():
             param.requires_grad = False
-
 
     def train(self, model):
         state = self.states.read_sensor_data
@@ -98,12 +87,5 @@ class Inference(Node):
         self.ddpg_model.update(state, action, reward, next_state, terminal_condition)
 
 
-def main(args=None):
-    rclpy.init()
-    inference = Inference()
-    rclpy.spin(inference)
-    inference.destroy_node()
-    rclpy.shutdown()
-
 if __name__ == '__main__':
-    main()
+    Inference()
