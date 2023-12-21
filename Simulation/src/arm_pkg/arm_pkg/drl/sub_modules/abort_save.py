69db44e0881fadbb06b1d581671462f2dba4ec58
8kyu <guillemsenabre@gmail.com>
2023-12-21 15:29:41 +0800

    changes in mg996rlocation | finished null_velocity method

--- Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/abort_save.py
+++ Simulation/src/arm_pkg/arm_pkg/drl/sub_modules/abort_save.py
@@ -8,25 +8,31 @@ class AbortOrSave():
         self.states = States()
         self.config = Configuration()
         self.angles = self.states.read_sensor_data[:10]
+        self.velocity_history = []
 
-        self.velocity = []
-
-    def reached_goal(self):
-
-        average_angle = sum(self.angles)/len(self.angles)
-        count = 0
-
-        while count <= self.config.number_of_values:
-            # Append average values until a certain threshold,
-            #Run out of brain today
-            self.angles.append
-
-
-            count += 1
-            average_angle = 0
+    def _null_velocity(self):
+        # Calculate the average angle
+        average_angle = sum(self.angles) / 10
         
-        # Velocity ~ 0
-        # Distance < threshold
+        # Append the average angle to the history
+        self.velocity_history.append(average_angle)
 
-    def local_minimum(self):
+        # Check if the history has reached the desired length
+        if len(self.velocity_history) == self.config.number_of_values:
+            # Check if the first, middle, and last items are the same
+            if self.velocity_history[0] == self.velocity_history[len(self.velocity_history)//2] == self.velocity_history[-1]:
+                return True
+
+            # Reset the history for the next round
+            self.velocity_history = []
+
+        return False 
+
+    def _local_minimum(self):
         pass
+
+    def terminal_condition(self):
+        if self._null_velocity() or self._local_minimum():
+            return True
+        else:
+            return False
