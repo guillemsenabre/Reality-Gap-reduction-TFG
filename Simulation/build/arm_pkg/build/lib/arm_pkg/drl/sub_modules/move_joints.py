ec9e0866b087642c2e1e4090d7778931671d0fa5
8kyu <guillemsenabre@gmail.com>
2023-12-24 15:49:11 +0800

    tf

---
+++ Simulation/build/arm_pkg/build/lib/arm_pkg/drl/sub_modules/move_joints.py
@@ -0,0 +1,30 @@
+import time
+import serial
+import struct
+from sub_modules.configuration import Configuration
+
+class MoveJoints:
+    def __init__(self):
+        print("Initializing Move Joints module eiaau let's movee")
+        self.config = Configuration()
+
+        time.sleep(0.3)
+
+    def move_joints(self, actions):
+
+        ser = serial.Serial(self.config.port1, baudrate=115200, timeout=1)
+
+        if len(actions)%2 != 0:
+            print("Error: there has to be an even amount of values")
+            return
+
+        try:
+            self._send_list(ser, actions)
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            ser.close()
+
+    def _send_list(self, ser, values):
+        ser.write(struct.pack(f'!{self.config.number_motors}f', *values))
+        print(f"Sent values to {self.config.port1}: {values}")
