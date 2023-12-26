6ba6ec3d2c36fabfe7d5e81d435afca67665c242
8kyu <guillemsenabre@gmail.com>
2023-12-26 20:29:59 +0800

    added method in move_joints.py to send a trigger signal al reset the motors

--- Implementation/inference/sub_modules/move_joints.py
+++ Implementation/inference/sub_modules/move_joints.py
@@ -10,19 +10,23 @@ class MoveJoints:
 
     def move_joints(self, actions, port, number_motors=10):
 
-        ser = serial.Serial(port, baudrate=115200, timeout=1)
+        self.ser = serial.Serial(port, baudrate=115200, timeout=1)
 
         if len(actions)%2 != 0:
             print("Error: there has to be an even amount of values")
             return
 
         try:
-            self._send_list(ser, actions, number_motors)
+            self._send_list(self.ser, actions, number_motors)
         except Exception as e:
             print(f"Error: {e}")
         finally:
-            ser.close()
+            self.ser.close()
 
     def _send_list(self, ser, values, port, number_motors):
         ser.write(struct.pack(f'!{number_motors}f', *values))
         print(f"Sent values to {port}: {values}")
+
+    def reset_motors(self):
+        self.ser.write(b'motors init\n')
+
